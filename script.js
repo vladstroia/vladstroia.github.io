2e80b18bc65b96d7f582f1e469f6b840fde0ddfd
vlastroia <vlad.stroia@gmail.com>
2019-10-09 21:38:24 +0300

    keep track of serve (does not work at advantage)

--- script.js
+++ script.js
@@ -4,55 +4,74 @@ scor_rares = 0;
 scor_vlad = 0;
 meci_rares = 0;
 meci_vlad = 0;
+serva = 0; // cine serveste; 0 daca serveste rares; 1 daca serveste vlad
 
 function verifica(){
 
 	if((scor_vlad - scor_rares >= 2) && (scor_vlad >= 11)){
 		scor_vlad=0;
 		scor_rares=0;
-		meci_vlad += 1;	
+		meci_vlad += 1;
+		serva = 1;
   }	
     if((scor_rares - scor_vlad >= 2) && (scor_rares >= 11)){
 		scor_vlad=0;
 		scor_rares=0;
-		meci_rares += 1;	
+		meci_rares += 1;
+		serva = 0;
   }	
     if(scor_vlad == 6 && scor_rares == 0){
 		scor_vlad=0;
 		scor_rares=0;
 		meci_vlad += 1;	
+		serva = 1;
 	}
     if(scor_vlad == 9 && scor_rares == 1){
 		scor_vlad=0;
 		scor_rares=0;
-		meci_vlad += 1;	
+		meci_vlad += 1;
+		serva = 1;
 	}
     if(scor_vlad == 0 && scor_rares == 6){
 		scor_vlad=0;
 		scor_rares=0;
-		meci_rares += 1;	
+		meci_rares += 1;
+		serva = 0;
 	}
 
     if(scor_vlad == 1 && scor_rares == 9){
 		scor_vlad=0;
 		scor_rares=0;
 		meci_rares += 1;	
+		serva = 0;
 	}
 
+	if((scor_vlad + scor_rares)%5 == 0 && scor_rares != 0 && scor_vlad != 0)
+		serva = Math.abs(serva-1)
 }
 function afiseaza(){
-
 	document.getElementById("rares").innerText=scor_rares;
     document.getElementById("vlad").innerText=scor_vlad;
 	document.getElementById("mrares").innerText=meci_rares;
     document.getElementById("mvlad").innerText=meci_vlad;
+	if (serva == 0){
+	  document.getElementById("rares").style.color = "red";
+	  document.getElementById("vlad").style.color = "white";
+	}
+	else{
+	  document.getElementById("rares").style.color = "white";
+	  document.getElementById("vlad").style.color = "red";
+	}
+
+
 }
 
 function WhichButton(event) {
   if(event.button == 0)
 	scor_rares += 1;
   else
-	scor_vlad += 1;
+	if(event.button == 1||event.button == 2)
+	  scor_vlad += 1;
   verifica();
   afiseaza();
 
@@ -60,10 +79,10 @@ function WhichButton(event) {
 
 function keyDownTextField(e) {
   var keyCode = e.keyCode;
- if(keyCode==37 || keyCode==0) {//37 e pt sageata stange
+ if(keyCode==37) {//37 e pt sageata stange
 	scor_rares += 1;
   } else 
-	  if(keyCode==39 || keyCode==2){//sageata dreapta
+	  if(keyCode==39){//sageata dreapta
 		  scor_vlad += 1;
 	  }
 	    else
